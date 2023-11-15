f8d0bf9092e287f456661e381276157b45bc41c0
kirtash93 <kirtash93@proton.me>
2023-11-15 10:34:57 +0100

    refactors

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import csv
 import etherscan
 import time
 import json
+import os
+import argparse
 from datetime import datetime, timedelta
+import sched
+import time
+from dotenv import load_dotenv
 
-CSV_FILE_PATH = "round_128.csv"
+CSV_FILE_PATH = "in/test.csv"
 BURN_ADDRESS = "0x0000000000000000000000000000000000000000"
 DONUT_CONTRACT = "0xC0F9bD5Fa5698B6505F643900FFA515Ea5dF54A9"
 
@@ -16,14 +21,19 @@ SUNSET_TIMESTAMP = SUNSET_DT.timestamp()
 
 SLEEP_TIME_SEC = 1
 API_LIMIT = 5
-API_KEY = "WHHQF6XHN4YDRY81MCV4ZWCGWU5IWX9B3W"
 
 
-def calculate_data(name, blockchain_address, donut_transactions):
+def calculate_special_membership(
+    name,
+    blockchain_address,
+    donut_transactions,
+    limit_timestamp,
+):
     total_donuts_burned = 0
     total_special_memberships_days = 0
     active_membership_timestamp = 0
     has_bought_membership = False
+    has_active_membership = False
     for transaction in donut_transactions:
         if transaction["to"] == BURN_ADDRESS:
             has_bought_membership = True
@@ -34,7 +44,8 @@ def calculate_data(name, blockchain_address, donut_transactions):
             end_txn_sp_memb_dt = txn_dt + timedelta(days=tx_special_memberships_days)
             end_txn_sp_memb_dt = end_txn_sp_memb_dt.timestamp()
 
-            if end_txn_sp_memb_dt >= SUNSET_TIMESTAMP:
+            if end_txn_sp_memb_dt >= limit_timestamp:
+                has_active_membership = True
                 total_special_memberships_days += tx_special_memberships_days
                 if (
                     active_membership_timestamp == 0
@@ -42,34 +53,81 @@ def calculate_data(name, blockchain_address, donut_transactions):
                 ):
                     active_membership_timestamp = transaction["timestamp"]
 
-    if has_bought_membership and active_membership_timestamp != 0:
+    reddit_user = {
+        "username": name,
+        "address": blockchain_address,
+        "activeMembership": has_active_membership,
+        "membershipRank": int(total_special_memberships_days / 30),
+        "totalDonutsBurned": total_donuts_burned,
+        "totalBoughtMonthsMembership": total_donuts_burned / COST_PER_MONTH,
+    }
+
+    if has_bought_membership and has_active_membership:
         spent_days = (
             SUNSET_DT - datetime.utcfromtimestamp(active_membership_timestamp)
         ).days
         not_spent_days = total_special_memberships_days - spent_days
-        sp_memb_until = SUNSET_DT + timedelta(days=not_spent_days)
 
-        return {
-            "username": name,
-            "address": blockchain_address,
-            "currentMembershipRank": int(spent_days / 30),
-            "totalDonutsBurned": total_donuts_burned,
-            "totalMonthsMembership": total_donuts_burned / COST_PER_MONTH,
-            "spentDays": spent_days,
-            "notSpentDays": not_spent_days,
-            "specialMembershipUntil": sp_memb_until.strftime("%Y-%m-%d"),
-        }
+        reddit_user.update(
+            {
+                "spentDays": spent_days,
+                "notSpentDays": not_spent_days,
+            }
+        )
+
+        return reddit_user
+
+    if has_bought_membership and not has_active_membership:
+        reddit_user.update(
+            {
+                "spentDays": total_special_memberships_days,
+                "notSpentDays": 0,
+            }
+        )
+        return reddit_user
+
     return None
 
 
-def main():
+def get_limit_timestamp(input_data):
+    if "isRedditSunset" in input_data and input_data["isRedditSunset"]:
+        return SUNSET_TIMESTAMP
+    else:
+        current_utc_datetime = datetime.utcnow()
+        return current_utc_datetime.timestamp()
+
+
+def create_and_save_data(sp_memb_data, input_data):
+    file_name = (
+        input_data["fileName"] if "fileName" in input_data else "special_memberships"
+    )
+    folder_path = os.getcwd() + "/out/"
+    os.makedirs(folder_path, exist_ok=True)
+    file_path = os.path.join(folder_path, file_name + ".json")
+    print(sp_memb_data)
+    with open(file_path, "w") as file:
+        json.dump(sp_memb_data, file, indent=2)
+
+    print(f"JSON data has been written to {file_path}")
+
+
+def freeze_process(api_calls):
+    if api_calls >= API_LIMIT:
+        print(f"API Limit {API_LIMIT} reached, waiting {SLEEP_TIME_SEC} second.")
+        time.sleep(SLEEP_TIME_SEC)
+        return 0
+    return api_calls
+
+
+def main(input_data):
     es = etherscan.Client(
-        api_key=API_KEY,
+        api_key="WHHQF6XHN4YDRY81MCV4ZWCGWU5IWX9B3W",
         cache_expire_after=5,
     )
 
     sp_memb_data = []
     api_calls = 0
+
     with open(CSV_FILE_PATH, "r") as file:
         csv_reader = csv.DictReader(file)
 
@@ -77,12 +135,7 @@ def main():
             name = row["username"]
             blockchain_address = row["blockchain_address"]
 
-            if api_calls >= API_LIMIT:
-                print(
-                    f"API Limit {API_LIMIT} reached, waiting {SLEEP_TIME_SEC} second."
-                )
-                time.sleep(SLEEP_TIME_SEC)
-                api_calls = 0
+            api_calls = freeze_process(api_calls)
 
             print(f"Retrieving data from: {name}")
             donut_transactions = es.get_token_transactions(
@@ -92,18 +145,54 @@ def main():
             api_calls += 1
 
             if donut_transactions:
-                reddit_user = calculate_data(
-                    name, blockchain_address, donut_transactions
+                reddit_user = calculate_special_membership(
+                    name,
+                    blockchain_address,
+                    donut_transactions,
+                    get_limit_timestamp(input_data),
                 )
                 if reddit_user:
                     sp_memb_data.append(reddit_user)
 
-    file_name = "output.json"
-
-    with open(file_name, "w") as file:
-        json.dump(sp_memb_data, file, indent=2)
-
-    print(f"JSON data has been written to {file_name}")
+    create_and_save_data(sp_memb_data, input_data)
 
 
-main()
+load_dotenv()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process input data")
+
+    parser.add_argument("input_variable", help="Input variable in JSON format")
+
+    args = parser.parse_args()
+
+    try:
+        input_data = json.loads(args.input_variable)
+        main(input_data)
+
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}")
+
+
+# Function to be executed periodically
+def periodic_task():
+    print("Executing the periodic task")
+    main(
+        {
+            "isRedditSunset": False,
+        }
+    )
+
+
+# Create a scheduler
+scheduler = sched.scheduler(time.time, time.sleep)
+
+# Schedule the task to run every 1 minute
+interval = 600  # in seconds
+scheduler.enter(interval, 1, periodic_task, ())
+
+# Run the scheduler
+try:
+    while True:
+        scheduler.run()
+except KeyboardInterrupt:
+    print("\nScript terminated by the user.")
